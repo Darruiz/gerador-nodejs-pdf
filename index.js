bd126f50340de783ace84ee5d256beb6d76a5219
Darruiz <99142969+Darruiz@users.noreply.github.com>
2024-04-11 07:37:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const app = express();
+const path = require('path');
+const fs = require('fs'); 
 const port = 3000;
 
-app.get('/gerar-pdf/:nome', async (req, res) => {
-    const browser = await puppeteer.launch();
+app.get('/servers/pdf/gerar-pdf/:nome', async (req, res) => {
+    console.log("caiu aqui");
+    const browser = await puppeteer.launch({
+        executablePath: '/usr/bin/google-chrome',
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+
     const page = await browser.newPage();
 
-    var nome = req.params.nome;
+    var nome = req.params.nome.split('.')[0];
 
-    await page.goto('http://localhost/jvaz/pdfs/' + nome, {waitUntil: 'networkidle2'});
+    await page.goto('#' + nome + '.html', {waitUntil: 'networkidle2'});
 
-    const path = 'pdfs/' + nome + '.pdf';
+    const filePath = path.join('pdfs', nome + '.pdf');
 
-    await page.pdf({path: path, format: 'A4'});
+    await page.pdf({path: filePath, format: 'A4'});
 
     await browser.close();
 
-    res.send("PDF gerado com sucesso. Salvo em: " + path);
+    res.send("1x1");
+});
+
+app.get('/servers/pdf/download-pdf/:nome', (req, res) => {
+    const nome = req.params.nome;
+    const pdfPath = path.join(__dirname, 'pdfs', nome);
+    
+    fs.exists(pdfPath, (exists) => {
+        if (exists) {
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader('Content-Disposition', `inline; filename="${nome}"`); 
+            
+            res.sendFile(pdfPath);
+        } else {
+            res.status(404).send('Não foi possível encontrar o arquivo PDF especificado.');
+        }
+    });
 });
 
 app.listen(port, () => {
